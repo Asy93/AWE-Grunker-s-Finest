23ef4aa6fd5399af8783c20668898200ba8f438f
Asy93 <90063869+Asy93@users.noreply.github.com>
2023-02-17 22:37:07 -0500

    Update AryxHeavyMissilePoseidon_Weapon.cs

--- Data/Scripts/CoreParts/AryxHeavyMissilePoseidon_Weapon.cs
+++ Data/Scripts/CoreParts/AryxHeavyMissilePoseidon_Weapon.cs
@@ -49,7 +49,7 @@ namespace Scripts {
                 MinimumDiameter = 0, // Minimum radius of threat to engage.
                 MaximumDiameter = 0, // Maximum radius of threat to engage; 0 = unlimited.
                 MaxTargetDistance = 0, // Maximum distance at which targets will be automatically shot at; 0 = unlimited.
-                MinTargetDistance = 0, // Minimum distance at which targets will be automatically shot at.
+                MinTargetDistance = 1500, // Minimum distance at which targets will be automatically shot at.
                 TopTargets = 4, // Maximum number of targets to randomize between; 0 = unlimited.
                 TopBlocks = 4, // Maximum number of blocks to randomize between; 0 = unlimited.
                 StopTrackingSpeed = 0, // Do not track threats traveling faster than this speed; 0 = unlimited.
