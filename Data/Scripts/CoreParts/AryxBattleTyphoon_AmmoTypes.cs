4ba849733f4a18a657e282a6643eef0a18db8a56
Starfleck <130235537+Starfleck@users.noreply.github.com>
2023-08-13 17:11:57 -0500

    Update AryxBattleTyphoon_AmmoTypes.cs

--- Data/Scripts/CoreParts/AryxBattleTyphoon_AmmoTypes.cs
+++ Data/Scripts/CoreParts/AryxBattleTyphoon_AmmoTypes.cs
@@ -146,10 +146,10 @@ namespace Scripts
                 {
                     Enable = true,
                     Radius = 8f, // Meters
-                    Damage = (float)(47000 * AWEGlobalDamageScalar),
+                    Damage = (float)(61000 * AWEGlobalDamageScalar),
                     Depth = 2f,
                     MaxAbsorb = 0f,
-                    Falloff = Linear, //.NoFalloff applies the same damage to all blocks in radius
+                    Falloff = InvCurve, //.NoFalloff applies the same damage to all blocks in radius
                     //.Linear drops evenly by distance from center out to max radius
                     //.Curve drops off damage sharply as it approaches the max radius
                     //.InvCurve drops off sharply from the middle and tapers to max radius
