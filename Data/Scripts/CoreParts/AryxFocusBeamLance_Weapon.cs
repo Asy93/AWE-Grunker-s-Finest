f960bd65a957916dc7cf8f2683666747155b631c
Starfleck <130235537+Starfleck@users.noreply.github.com>
2023-08-19 01:31:41 -0500

    Update AryxFocusBeamLance_Weapon.cs

--- Data/Scripts/CoreParts/AryxFocusBeamLance_Weapon.cs
+++ Data/Scripts/CoreParts/AryxFocusBeamLance_Weapon.cs
@@ -114,7 +114,7 @@ namespace Scripts
                     BarrelsPerShot = 1,
                     TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
                     SkipBarrels = 0,
-                    ReloadTime = 550, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 500, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 100, //10 heat generated per shot
                     MaxHeat = 24400, //max heat before weapon enters cooldown (70% of max heat)
