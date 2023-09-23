fd4a987a57fe44db75ea0c4f33e413e9b1152266
Starfleck <130235537+Starfleck@users.noreply.github.com>
2023-09-23 09:45:27 -0500

    added missing resistance factor

--- Data/Scripts/CoreParts/AryxPlasmaHelios_Weapon.cs
+++ Data/Scripts/CoreParts/AryxPlasmaHelios_Weapon.cs
@@ -23,6 +23,7 @@ namespace Scripts
                         MuzzlePartId = "None",
                         AzimuthPartId = "None",
                         ElevationPartId = "None",
+                        DurabilityMod = 0.25f,
                     },
 
                 },
