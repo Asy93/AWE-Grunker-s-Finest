a8f5bd00abd0124e186983e5df7b11f56d2f166e
SupremeAdmiralZ <103025241+SupremeAdmiralZ@users.noreply.github.com>
2023-12-10 14:01:59 -0800

    Update AryxPDCGuardian_Weapon.cs

--- Data/Scripts/CoreParts/AryxPDCGuardian_Weapon.cs
+++ Data/Scripts/CoreParts/AryxPDCGuardian_Weapon.cs
@@ -92,8 +92,8 @@ namespace Scripts
                 },
                 HardWare = new HardwareDef
                 {
-                    RotateRate = 0.11f,
-                    ElevateRate = 0.11f,
+                    RotateRate = 0.5f,
+                    ElevateRate = 0.5f,
                     MinAzimuth = -180,
                     MaxAzimuth = 180,
                     MinElevation = -10,
@@ -130,7 +130,7 @@ namespace Scripts
                     BarrelsPerShot = 1,
                     TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
                     SkipBarrels = 0,
-                    ReloadTime = 300, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 30, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 0, //heat generated per shot
                     MaxHeat = 70000, //max heat before weapon enters cooldown (70% of max heat)
