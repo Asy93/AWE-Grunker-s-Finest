a13f45141f471a9c0d91f2bcff8874f6f66a48de
SupremeAdmiralZ <103025241+SupremeAdmiralZ@users.noreply.github.com>
2023-12-10 14:58:49 -0800

    Update AryxBattleTyphoon_AmmoTypes.cs

--- Data/Scripts/CoreParts/AryxBattleTyphoon_AmmoTypes.cs
+++ Data/Scripts/CoreParts/AryxBattleTyphoon_AmmoTypes.cs
@@ -33,7 +33,7 @@ namespace Scripts
             HybridRound = false, //AmmoMagazine based weapon with energy cost
             EnergyCost = 0.00000000000f, //(((EnergyCost * DefaultDamage) * ShotsPerSecond) * BarrelsPerShot) * ShotsPerBarrel
             BaseDamage = (float)(2000 * AWEGlobalDamageScalar),
-            Mass = 30000f, // in kilograms
+            Mass = 1000f, // in kilograms
             Health = 0, // 0 = disabled, otherwise how much damage it can take from other trajectiles before dying.
             BackKickForce = 300000,
             DecayPerShot = 0,
