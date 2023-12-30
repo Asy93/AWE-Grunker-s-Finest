985ff6574b9c54c8d249fc9aa334f410e80196b0
SupremeAdmiralZ <103025241+SupremeAdmiralZ@users.noreply.github.com>
2023-12-29 21:55:59 -0800

    Update AryxBurstPunisher_AmmoTypes.cs

--- Data/Scripts/CoreParts/AryxBurstPunisher_AmmoTypes.cs
+++ Data/Scripts/CoreParts/AryxBurstPunisher_AmmoTypes.cs
@@ -230,8 +230,8 @@ namespace Scripts
                 TargetLossTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 MaxLifeTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 AccelPerSec = 0f,
-                DesiredSpeed = 1200, // voxel phasing if you go above 5100
-                MaxTrajectory = 3000f,
+                DesiredSpeed = 2400, // voxel phasing if you go above 5100
+                MaxTrajectory = 6000f,
                 //FieldTime was here, it's dead now is disabled, a value causes the projectile to come to rest, spawn a field and remain for a time (Measured in game ticks, 60 = 1 second)
                 GravityMultiplier = 0f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable.
                 SpeedVariance = Random(start: 0, end: 0), // subtracts value from DesiredSpeed
