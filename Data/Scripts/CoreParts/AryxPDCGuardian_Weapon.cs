f11e5786dc8d2bbb11d27349cb841c4498f32477
SupremeAdmiralZ <103025241+SupremeAdmiralZ@users.noreply.github.com>
2024-01-27 13:47:23 -0800

    Update AryxPDCGuardian_Weapon.cs

--- Data/Scripts/CoreParts/AryxPDCGuardian_Weapon.cs
+++ Data/Scripts/CoreParts/AryxPDCGuardian_Weapon.cs
@@ -59,11 +59,11 @@ namespace Scripts
                 LockedSmartOnly = false, // Only fire at smart projectiles that are locked on to parent grid.
                 MinimumDiameter = 0, // 0 = unlimited, Minimum radius of threat to engage.
                 MaximumDiameter = 0, // 0 = unlimited, Maximum radius of threat to engage.
-                MaxTargetDistance = 0, // 0 = unlimited, Maximum target distance that targets will be automatically shot at.
+                MaxTargetDistance = 1600, // 0 = unlimited, Maximum target distance that targets will be automatically shot at.
                 MinTargetDistance = 0, // 0 = unlimited, Min target distance that targets will be automatically shot at.
-                TopTargets = 6, // 0 = unlimited, max number of top targets to randomize between.
+                TopTargets = 0, // 0 = unlimited, max number of top targets to randomize between.
                 TopBlocks = 0, // 0 = unlimited, max number of blocks to randomize between
-                StopTrackingSpeed = 0, // do not track target threats traveling faster than this speed
+                StopTrackingSpeed = 10000, // do not track target threats traveling faster than this speed
             },
             HardPoint = new HardPointDef
             {
