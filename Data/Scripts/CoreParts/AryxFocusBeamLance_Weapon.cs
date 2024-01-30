3fd3110c49ee04a60e8d99eaea77877effffeb04
SupremeAdmiralZ <103025241+SupremeAdmiralZ@users.noreply.github.com>
2024-01-29 23:10:30 -0800

    Update AryxFocusBeamLance_Weapon.cs

--- Data/Scripts/CoreParts/AryxFocusBeamLance_Weapon.cs
+++ Data/Scripts/CoreParts/AryxFocusBeamLance_Weapon.cs
@@ -24,7 +24,7 @@ namespace Scripts
                         MuzzlePartId = "None",
                         AzimuthPartId = "None",
                         ElevationPartId = "None",
-                        DurabilityMod = 0.125f,
+                        DurabilityMod = 0.167f,
                     },
 
                 },
