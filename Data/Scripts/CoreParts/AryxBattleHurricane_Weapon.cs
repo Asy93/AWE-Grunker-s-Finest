2a2a5c878739eac1c496b8f722744b99316e6e55
SupremeAdmiralZ <103025241+SupremeAdmiralZ@users.noreply.github.com>
2024-01-29 22:25:12 -0800

    Update AryxBattleHurricane_Weapon.cs

--- Data/Scripts/CoreParts/AryxBattleHurricane_Weapon.cs
+++ Data/Scripts/CoreParts/AryxBattleHurricane_Weapon.cs
@@ -116,7 +116,7 @@ namespace Scripts
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 500, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 450, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 2, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 0, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 0, // Heat generated per shot.
